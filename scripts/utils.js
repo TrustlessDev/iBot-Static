4974cc881db11f65d9a4bb365eb9e63a492bff02
JiangJhong <happygo0505@gmail.com>
2023-08-10 23:27:56 +0800

    index loader delay 300ms

--- scripts/utils.js
+++ scripts/utils.js
@@ -22,7 +22,6 @@ function closePreloader() {
 }
 
 async function initSite() {
-    console.log("initSite");
     $("#ibot-preloader h2").html("Loading...");
     $("#ibot-preloader").show();
     try {
