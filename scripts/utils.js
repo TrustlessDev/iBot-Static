6c1e40a9685672fdc7a797b352e21b655748cb92
JiangJhong <happygo0505@gmail.com>
2023-08-12 14:21:18 +0800

    fix

--- scripts/utils.js
+++ scripts/utils.js
@@ -33,8 +33,7 @@ async function initSite() {
             }
         });
         let data = await resp.json();
-        alert(url.host);
-        eruda.init();
+        //eruda.init();
         if(data[url.host]) {
             site = data[url.host];
         } else {
