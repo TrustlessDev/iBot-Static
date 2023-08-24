4e9f24781c4ea9e8d78e7a14c1f19f5d941ec5fc
JiangJhong <happygo0505@gmail.com>
2023-08-24 19:43:50 +0800

    Test

--- scripts/prodAdd.js
+++ scripts/prodAdd.js
@@ -3,20 +3,24 @@ function initAddProductPage() {
     setTimeout(async function () {
         await loadJS("scripts/utils.js");
         await initSite();
-        var stepperAdd = document.querySelectorAll('.stepper-add');
-        var stepperSub = document.querySelectorAll('.stepper-sub');
-        if(stepperAdd.length){
-            stepperAdd.forEach(el => el.addEventListener('click', event => {
-                var currentValue = el.parentElement.querySelector('input').value;
-                el.parentElement.querySelector('input').value = +currentValue + 1;
-            }))
+        try {
+            var stepperAdd = document.querySelectorAll('.stepper-add');
+            var stepperSub = document.querySelectorAll('.stepper-sub');
+            if(stepperAdd.length){
+                stepperAdd.forEach(el => el.addEventListener('click', event => {
+                    var currentValue = el.parentElement.querySelector('input').value;
+                    el.parentElement.querySelector('input').value = +currentValue + 1;
+                }))
 
-            stepperSub.forEach(el => el.addEventListener('click', event => {
-                var currentValue = el.parentElement.querySelector('input').value;
-                if(currentValue > 1) {
-                    el.parentElement.querySelector('input').value = +currentValue - 1;
-                }
-            }))
+                stepperSub.forEach(el => el.addEventListener('click', event => {
+                    var currentValue = el.parentElement.querySelector('input').value;
+                    if(currentValue > 1) {
+                        el.parentElement.querySelector('input').value = +currentValue - 1;
+                    }
+                }))
+            }
+        }catch(e) {
+            console.log(e);
         }
         // 載入圖片
         $("#image-data").attr("src", "images/ad-demo.jpg");
