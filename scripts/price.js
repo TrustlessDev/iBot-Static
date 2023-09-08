94b36f0abcee868d791caf7cc45a04076dcb2082
JiangJhong <happygo0505@gmail.com>
2023-09-08 19:48:47 +0800

    setup

--- scripts/price.js
+++ scripts/price.js
@@ -2,20 +2,17 @@ let ws = null;
 
 function setupWebSocket() {
     ws = new WebSocket('ws://' + site.apiUrl + '/ws');
-
+    console.log('Connecting to WebSocket...');
     ws.onopen = () => {
         console.log('Connected to WebSocket');
     };
-
     ws.onmessage = (event) => {
         console.log('Received from server:', event.data);
     };
-
     ws.onclose = (event) => {
         console.log('WebSocket closed. Reconnecting in 3 seconds...', event);
         setTimeout(setupWebSocket, 3000); // 如果連接關閉，則在3秒後重試
     };
-
     ws.onerror = (error) => {
         console.error('WebSocket Error:', error);
     };
