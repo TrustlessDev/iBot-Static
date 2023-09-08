552d0c3be70699a53a57e51ea27b068fcb9a24c0
JiangJhong <happygo0505@gmail.com>
2023-09-08 19:57:37 +0800

    wss

--- scripts/price.js
+++ scripts/price.js
@@ -1,24 +1,21 @@
 let ws = null;
 
 async function setupWebSocket() {
-    return new Promise((resolve, reject) => {
-        let ws = new WebSocket('ws://' + site.apiUrl + '/ws');
-        console.log('Connecting to WebSocket...');
-        ws.onopen = () => {
-            console.log('Connected to WebSocket');
-            resolve(ws);
-        };
-        ws.onmessage = (event) => {
-            console.log('Received from server:', event.data);
-        };
-        ws.onclose = (event) => {
-            console.log('WebSocket closed. Reconnecting in 3 seconds...', event);
-            setTimeout(setupWebSocket, 3000); // 如果連接關閉，則在3秒後重試
-        };
-        ws.onerror = (error) => {
-            console.error('WebSocket Error:', error);
-        };
-    });
+    ws = new WebSocket('wss://' + site.apiUrl + '/ws');
+    ws.onopen = () => {
+        console.log('Connected to WebSocket');
+        sendMessage();
+    };
+    ws.onmessage = (event) => {
+        console.log('Received from server:', event.data);
+    };
+    ws.onclose = (event) => {
+        console.log('WebSocket closed. Reconnecting in 3 seconds...', event);
+        setTimeout(setupWebSocket, 3000); // 如果連接關閉，則在3秒後重試
+    };
+    ws.onerror = (error) => {
+        console.error('WebSocket Error:', error);
+    };
 }
 
 function sendMessage() {
