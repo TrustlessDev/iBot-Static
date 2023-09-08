66343ccbd1cf509db3d4c423158c0215f48303e4
JiangJhong <happygo0505@gmail.com>
2023-09-08 19:50:14 +0800

    ws promise

--- scripts/price.js
+++ scripts/price.js
@@ -1,21 +1,24 @@
 let ws = null;
 
-function setupWebSocket() {
-    ws = new WebSocket('ws://' + site.apiUrl + '/ws');
-    console.log('Connecting to WebSocket...');
-    ws.onopen = () => {
-        console.log('Connected to WebSocket');
-    };
-    ws.onmessage = (event) => {
-        console.log('Received from server:', event.data);
-    };
-    ws.onclose = (event) => {
-        console.log('WebSocket closed. Reconnecting in 3 seconds...', event);
-        setTimeout(setupWebSocket, 3000); // 如果連接關閉，則在3秒後重試
-    };
-    ws.onerror = (error) => {
-        console.error('WebSocket Error:', error);
-    };
+async function setupWebSocket() {
+    return new Promise((resolve, reject) => {
+        let ws = new WebSocket('ws://' + site.apiUrl + '/ws');
+        console.log('Connecting to WebSocket...');
+        ws.onopen = () => {
+            console.log('Connected to WebSocket');
+            resolve(ws);
+        };
+        ws.onmessage = (event) => {
+            console.log('Received from server:', event.data);
+        };
+        ws.onclose = (event) => {
+            console.log('WebSocket closed. Reconnecting in 3 seconds...', event);
+            setTimeout(setupWebSocket, 3000); // 如果連接關閉，則在3秒後重試
+        };
+        ws.onerror = (error) => {
+            console.error('WebSocket Error:', error);
+        };
+    });
 }
 
 function sendMessage() {
