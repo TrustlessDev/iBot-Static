44938e1bae30ae8cbf7e082e9e09b280c9a9f692
JiangJhong <happygo0505@gmail.com>
2023-09-09 18:22:12 +0800

    30

--- scripts/price.js
+++ scripts/price.js
@@ -107,7 +107,7 @@ async function loadKChart() {
         };
     });
     // 只取最後 100 筆
-    sampleData = sampleData.slice(-40);
+    sampleData = sampleData.slice(-30);
     console.log(sampleData);
 
     // Sample data
