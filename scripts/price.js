842eebc27f70ebd63a6386e2945e240a164757b4
JiangJhong <happygo0505@gmail.com>
2023-09-14 06:05:40 +0800

    align

--- scripts/price.js
+++ scripts/price.js
@@ -208,12 +208,14 @@ async function loadDepthTable(symbol, precision = 0.1) {
             let tr = document.createElement("tr");
             let td1 = document.createElement("td");
             td1.classList.add("color-gray-light");
+            td1.classList.add("text-start");
             let td2 = document.createElement("td");
             td1.classList.add("color-green-light");
             let td3 = document.createElement("td");
             td1.classList.add("color-red-light");
             let td4 = document.createElement("td");
             td4.classList.add("color-gray-light");
+            td4.classList.add("text-end");
             td1.innerText = bidsTable[i].quantity.toFixed(6);
             // 價格依照 precision 顯示
             td2.innerText = parseFloat(bidsTable[i].price).toFixed(precision.toString().split(".")[1].length);
